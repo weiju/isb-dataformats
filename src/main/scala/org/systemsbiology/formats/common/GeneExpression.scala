72dafd6c02391bddffdb1fe2daed0dc217179f76
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-04-04 11:43:23 -0700

    moved MutableGeneExpressionMeasurement from Pebble to here

--- src/main/scala/org/systemsbiology/formats/common/GeneExpression.scala
+++ src/main/scala/org/systemsbiology/formats/common/GeneExpression.scala
@@ -6,10 +6,54 @@ package org.systemsbiology.formats.common
  */
 case class GeneExpressionValue(ratio: Double, lambda: Double)
 
+/**
+ * Interface for gene expression measurements.
+ */
 trait GeneExpressionMeasurement {
+
+  /**
+   * Returns this measurement's condition names.
+   * @return condition names
+   */
   def conditions: Array[String]
+
+  /**
+   * Returns this measurement's VNG gene names.
+   * @return VNG gene names
+   */
   def vngNames: Array[String]
+
+  /**
+   * Returns this measurement's real common gene names if possible.
+   * When there is no official name available, the name will fall back to the
+   * VNG name
+   * @return official gene names
+   */
   def geneNames: Array[String]
 
+  /**
+   * Retrieve the measurement value at the specified position.
+   * @param geneIndex gene name index
+   * @param condition index condition name index
+   * @return measurement value at the specified position
+   */
   def apply(geneIndex: Int, conditionIndex: Int): GeneExpressionValue
+}
+
+/**
+ * A class which can be used to combine measurements from various sources.
+ * @constructor creates a MutableGeneExpressionMeasurement instance
+ * @param vngNames the VNG names
+ * @param "real" gene names
+ * @param conditions the conditions
+ */
+class MutableGeneExpressionMeasurement(val vngNames: Array[String],
+                                       val geneNames: Array[String],
+                                       val conditions: Array[String])
+extends GeneExpressionMeasurement {
+  val data = Array.ofDim[GeneExpressionValue](vngNames.length, conditions.length)
+  def apply(geneIndex: Int, conditionIndex: Int) = data(geneIndex)(conditionIndex)
+  def update(geneIndex: Int, conditionIndex: Int, value: GeneExpressionValue) {
+    data(geneIndex)(conditionIndex) = value
+  }
 }
---
+++ src/test/scala/org/systemsbiology/formats/common/GeneExpressionMeasurementTest.scala
@@ -0,0 +1,31 @@
+package org.systemsbiology.formats.common
+
+/**
+ * Test cases for OligoMapDatabase
+ */
+import org.scalatest.{FlatSpec,BeforeAndAfterEach}
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import java.io._
+
+@RunWith(classOf[JUnitRunner])
+class GeneExpressionMeasurementSpec extends FlatSpec with ShouldMatchers with BeforeAndAfterEach {
+  var measurement : MutableGeneExpressionMeasurement = null
+
+  override def beforeEach {
+    measurement = new MutableGeneExpressionMeasurement(Array("VNG0001", "VNG0002"),
+                                                       Array("gene1", "gene2"),
+                                                       Array("condition1", "condition2", "condition3"))
+  }
+
+  "MutableGeneExpressionMeasurement" should "have its values initialized with null" in {
+    measurement(0, 0) should be (null)
+  }
+  it should "be possible to change the values" in {
+    val value = GeneExpressionValue(1.0, 2.0)
+    measurement(0, 0) = value
+    measurement(0, 0) should be (value)
+  }
+}
