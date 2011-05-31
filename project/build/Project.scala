ee10e1712ff2f3a5f29e7b85822daab6bf66a25f
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-05-31 10:40:49 -0700

    updated to latest scala, scalatest, sbt versions

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,6 +2,6 @@ import sbt._
 
 class IsbProject(info: ProjectInfo) extends DefaultProject(info) {
   val junit4 = "junit" % "junit" % "4.8.2" % "test"
-  val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
 }
 
